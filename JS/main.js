04ac3bb2ffb995c903ec31d6fbda258381e6973a
Chris2022 <ctejada415gmail.com>
2022-04-12 19:26:59 -0400

    changes to JS file, changes to sign up/login pages

--- JS/main.js
+++ JS/main.js
@@ -1,13 +1,69 @@
+// FAQ JS
 let question = document.getElementById("FAQ-main");
+let section = document.getElementById("quest");
 FAQ();
-function FAQ(e){
+function FAQ(e) {
     fetch("../JSON/FAQ.json")
-    .then((res) => res.json())
-    .then((data) => {
-        for (var i = 0; i < data.length; i++) {
-            var div = document.createElement("div");
-            div.innerHTML = data[i].question + ' '+ `<br>`+ data[i].Answer;
-            question.appendChild(div);
-          }
-    }).catch((err) => console.log(err));
+        .then((res) => res.json())
+        .then((data) => {
+            for (var i = 0; i < data.length; i++) {
+                var div = document.createElement("div");
+                div.innerHTML = data[i].question + " " + `<br>` + data[i].Answer;
+                div.className = "FAQ-question";
+                question.appendChild(div);
+            }
+        })
+        .catch((err) => console.log(err));
+}
+
+// JS for user LOGIN
+let form = document.getElementById("form");
+//if there is a form then add event listener
+if (form) form.addEventListener("submit", getCredentials);
+//function to get the credentials
+//then checks if form is filled, if it is then is redirects to the dashboard
+function getCredentials(e) {
+    e.preventDefault();
+    let user = document.getElementById("username").value;
+    let pass = document.getElementById("pass").value;
+    console.log(user, pass);
+    if (user === "" || pass === "") {
+        alert("Please fill in all the fields");
+    } else {
+        window.location.href = "../docs/dashboard.html";
+        alert("Welcome Back " + user);
+    }
+}
+
+//JS for user SIGNUP
+//Constructor to create a new user, takes in the first name, last name, username, password and birthday
+class User {
+    constructor(fName, lName, username, pass, birthday) {
+        this.fName = fName;
+        this.lName = lName;
+        this.username = username;
+        this.pass = pass;
+        this.birthday = birthday;
+    }
+}
+let signUp = document.getElementById("register");
+//if there is a form, then add an event listener
+if (signUp) signUp.addEventListener("submit", newUser);
+//function to add a new user
+function newUser(e) {
+    e.preventDefault();
+    let fName = document.getElementById("fName").value;
+    let lName = document.getElementById("lName").value;
+    let username = document.getElementById("username").value;
+    let pass = document.getElementById("pass").value;
+    let birthday = document.getElementById("birthday").value;
+    if (fName === "" ||lName === "" ||username === "" ||pass === "" ||birthday === "") {
+        alert("Please fill in all the fields");
+    } else {
+        //create a new user
+        const user = new User(fName, lName, username, pass, birthday);
+        console.log(user);
+        alert("Welcome to Task Bud" + ' ' +user.fName);
+        window.location.href = "../docs/login.html";
+    }
 }
