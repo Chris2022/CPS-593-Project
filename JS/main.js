81691fe32df2221715287029f4c712722e2a12b3
Chris2022 <ctejada415gmail.com>
2022-04-12 09:03:01 -0400

    addding node/npm/js

--- JS/main.js
+++ JS/main.js
@@ -1,21 +1,13 @@
 let question = document.getElementById("FAQ-main");
-
+FAQ();
 function FAQ(e){
-    e.preventdefault();
-    question.innerHTML= "";
     fetch("../JSON/FAQ.json")
     .then((res) => res.json())
     .then((data) => {
-        data.forEach(function(element){
-            let FAQ = document.createElement("div");
-            let question = document.createElement("h3");
-            let answer = document.createElement("p");
-            FAQ.className = "questions";
-            question.appendChild(document.createTextNode(element.question));
-            console.log(element.question);
-            answer.appendChild(document.createTextNode(element.answer));
-            FAQ.appendChild(question);
-            FAQ.appendChild(answer);
-        });
+        for (var i = 0; i < data.length; i++) {
+            var div = document.createElement("div");
+            div.innerHTML = data[i].question + ' '+ `<br>`+ data[i].Answer;
+            question.appendChild(div);
+          }
     }).catch((err) => console.log(err));
 }
